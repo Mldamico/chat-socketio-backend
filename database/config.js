570303a2e2d95382ed3dea39911341a1c250c60e
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-01-02 08:32:42 -0300

    DB connection with mongo

---
+++ database/config.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CNN_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('DB ONLINE');
+  } catch (error) {
+    throw new Error('Error en la base de datos');
+  }
+};
+
+module.exports = {
+  dbConnection,
+};
--- models/server.js
+++ models/server.js
@@ -1,51 +1,49 @@
 // Servidor de Express
-const express  = require('express');
-const http     = require('http');
+const express = require('express');
+const http = require('http');
 const socketio = require('socket.io');
-const path     = require('path');
-
-const Sockets  = require('./sockets');
+const path = require('path');
+const { dbConnection } = require('../database/config');
+const Sockets = require('./sockets');
 
 class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
 
-    constructor() {
+    dbConnection();
+    // Http server
+    this.server = http.createServer(this.app);
 
-        this.app  = express();
-        this.port = process.env.PORT;
+    // Configuraciones de sockets
+    this.io = socketio(this.server, {
+      /* configuraciones */
+    });
+  }
 
-        // Http server
-        this.server = http.createServer( this.app );
-        
-        // Configuraciones de sockets
-        this.io = socketio( this.server, { /* configuraciones */ } );
-    }
+  middlewares() {
+    // Desplegar el directorio público
+    this.app.use(express.static(path.resolve(__dirname, '../public')));
+  }
 
-    middlewares() {
-        // Desplegar el directorio público
-        this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
-    }
+  // Esta configuración se puede tener aquí o como propieda de clase
+  // depende mucho de lo que necesites
+  configurarSockets() {
+    new Sockets(this.io);
+  }
 
-    // Esta configuración se puede tener aquí o como propieda de clase
-    // depende mucho de lo que necesites
-    configurarSockets() {
-        new Sockets( this.io );
-    }
+  execute() {
+    // Inicializar Middlewares
+    this.middlewares();
 
-    execute() {
+    // Inicializar sockets
+    this.configurarSockets();
 
-        // Inicializar Middlewares
-        this.middlewares();
-
-        // Inicializar sockets
-        this.configurarSockets();
-
-        // Inicializar Server
-        this.server.listen( this.port, () => {
-            console.log('Server corriendo en puerto:', this.port );
-        });
-    }
-
+    // Inicializar Server
+    this.server.listen(this.port, () => {
+      console.log('Server corriendo en puerto:', this.port);
+    });
+  }
 }
-
 
 module.exports = Server;

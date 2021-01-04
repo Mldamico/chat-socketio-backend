d2af0e15eaa5cf9c93bc02b700b0dc6a6710e257
mldamico <matias.lioneldamico@gmail.com>
2021-01-04 10:42:20 -0300

    Validar jwt

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const validarJWT = async (req, res, next) => {
+  try {
+    const token = req.headers("x-token");
+    if (!token) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No hay token en la peticion"
+      });
+    }
+    const payload = jwt.verify(token, process.env.JWT_KEY);
+    req.uid = payload.uid;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Token no valido"
+    });
+  }
+};
+
+module.exports = { validarJWT };
--- router/auth.js
+++ router/auth.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const { crearUsuario, login, renewToken } = require("../controllers/auth");
 const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
 const router = Router();
 
 router.post(
@@ -31,6 +32,6 @@ router.post(
   login
 );
 
-router.get("/renew", renewToken);
+router.get("/renew", validarJWT, renewToken);
 
 module.exports = router;

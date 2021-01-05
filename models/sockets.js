9eee47c441430fe57e30dff0a89324b1b7e0a219
mldamico <matias.lioneldamico@gmail.com>
2021-01-05 16:23:45 -0300

    Socket controller

--- models/sockets.js
+++ models/sockets.js
@@ -1,3 +1,4 @@
+const { usuarioConectado } = require("../controllers/sockets");
 const { comprobarJWT } = require("../helpers/jwt");
 
 class Sockets {
@@ -9,15 +10,15 @@ class Sockets {
 
   socketEvents() {
     // On connection
-    this.io.on("connection", socket => {
+    this.io.on("connection", async socket => {
       const [valido, uid] = comprobarJWT(socket.handshake.query["x-token"]);
       if (!valido) {
         console.log("socket no identificado");
         return socket.disconnect();
       }
-      console.log("cliente conectado", uid);
-      socket.on("disconnect", () => {
-        console.log("cliente desconectado");
+      await usuarioConectado(uid);
+      socket.on("disconnect", async () => {
+        await usuarioDesconectado(uid);
       });
     });
   }
